a88a2c850e220e8e1305fb4577c432a01eea370b
DenNtp <Natthapon2557azsx@gmaile.com>
2022-03-12 22:49:24 +0700

    try to connect my sql

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const port = 3000
 const app = express()
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "http://www.srichomthong.com",
+    user: "master",
+    password: "1qaz2wsx3edc"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
 app.get('/',(req,res) => {
     res.send("Hello world")
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res) => {
-    res.send("Hello user")
+router.get('/test',(req,res) => {
+    res.send("Hello user")  
 })
